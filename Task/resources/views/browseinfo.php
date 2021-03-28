eb98f102cecdb93e7837ea592f2ce5da4d6c1be1
webforRahanuma <rahanumatasnim98@gmail.com>
2021-03-28 06:36:52 +0600

    browse

---
+++ Task/resources/views/browseinfo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/style.css">
+</head>
+<body>
+    <center>
+        <td width="100px" align="center">
+            <h2 align="center" >Seek Informations</h2>
+            <a href="https://google.com/">Google</a><br>
+            <a href="https://www.yahoo.com/">Yahoo</a><br>
+            <a href="https://www.wikipedia.org/">Wikipedia</a><br>
+            <a href="https://www.bing.com/">Bing</a>
+            <br/>
+            <br><a href="home_page.php"><input type="button" value=" Back"></a>
+        <a href="logout.php">logout</a><br>
+        </td>   
+    </center>
+</body>
+</html>
+
+
