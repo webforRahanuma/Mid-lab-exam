ba1501273a03620e52be8c1ae512822c2caaf143
webforRahanuma <rahanumatasnim98@gmail.com>
2021-03-28 06:01:43 +0600

    customercontroller

---
+++ Task/app/Http/Controllers/customerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class customerController extends Controller
+{
+    public function index(Request $req){
+
+        
+    }
+}
